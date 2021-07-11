ef8dcc4cdc74c2b50d6359285d7f413db83395f7
star-junk <storageforprojectfiles@gmail.com>
2021-07-11 10:54:14 +0000

    deployment setup init

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,21 @@
-FROM randomtools-v2
+FROM randomtools/randomtools-api:pyuwinx2
+# change user to root
 USER root
-RUN systemctl start nginx
-RUN systemctl start uwsgi_randomtools
-EXPOSE 80
+# change working directory
+WORKDIR /var/www/html/randomtools
+# remove existing files if any
+# RUN shopt -s dotglob
+RUN rm -rf ./*
+# copy current directory files
+COPY . .
+# install dependencies
+# COPY ./scripts/uwsgi_randomtools.service /etc/systemd/system/uwsgi_items_rest.service
+# COPY ./scripts/randomtools.conf /etc/nginx/sites-available/randomtools.conf
+# RUN ln -s /etc/nginx/sites-available/items-rest.conf /etc/nginx/sites-enabled/
+RUN mkdir logs
+RUN apt-get install python3-pip python3-dev libpq-dev
+RUN pip install -r requirements.txt
+# change permission of start script
+RUN chmod +rwx ./scripts/start.sh
+# start start.sh script which will start uwsgi and nginx
+CMD [ "./scripts/start.sh" ]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # Start nginx
-systemctl start nginx -D
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start nginx: $status"
-  exit $status
-fi
 
 # Start uwsgi
 systemctl start uwsgi_randomtools
@@ -14,16 +8,25 @@ status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start uwsgi: $status"
   exit $status
+fi 
+
+systemctl start nginx
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start nginx: $status"
+  exit $status
 fi
 
 while sleep 60; do
   ps aux |grep nginx |grep -q -v grep
   PROCESS_1_STATUS=$?
-  ps aux |grep uwsgi_randomtools |grep -q -v grep
+  ps aux |grep uwsgi |grep -q -v grep
   PROCESS_2_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
   if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+    echo $PROCESS_1_STATUS
+    echo $PROCESS_2_STATUS
     echo "One of the processes has already exited."
     exit 1
   fi
